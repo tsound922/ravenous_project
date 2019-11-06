cb5c5e34c74a85bcf62571f5176c8fd9cb4250a3
tsound922 <oushirx@gmail.com>
2019-11-07 03:40:20 +1100

    Search Bar's State test  complete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
-  city: 'Flavortown',
+  city: 'Bordertown',
   state: 'NY',
   zipCode: '10101',
   category: 'Italian',
@@ -15,16 +15,26 @@ const business = {
   reviewCount: 90
 };
 
-const businesses = [business, business, business, business, business, business];
-
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
 
 class App extends React.Component {
-  render(){
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
+  render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-         <SearchBar />
-         <BusinessList businesses = {businesses} />
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
